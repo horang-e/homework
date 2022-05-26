c047d73705d8b19364bb3d2e0fd29fd7f336307a
horang-e <rmsid1000@gmail.com>
2022-05-26 16:15:56 +0900

    체체종...ㅜㅜ

--- src/Home.js
+++ src/Home.js
@@ -1,58 +1,67 @@
 import React, { useState }  from "react";
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
-import Button from './Button';
-import Average from "./Average";
 
 
 
-const reset= () => { window.location.reload(false)}
+
 const Home = () => { 
+const randomNum = Array(7).fill().map(() => (Math.floor(Math.random() * 6)))
+const [rank, setRank] = useState(randomNum);
+let average = (randomNum.reduce((a,b)=>a+b,0)/7).toFixed(1)
+const [ave , setAve] = useState(average)
+const reset = () => {let a= 0.0;
+  setAve(a) }
+const reload = () => (window.location.reload(false))
   return (
-    <>
+    <> 
       <div className="App">
       <h2 style={{marginTop:"15px"}}>내 일주일은?</h2>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>일</h3></div>
-      <Rating />
-      <Link to="/dayrating/일요일"><Button /></Link>
-    </div>
+      <Rating rank={rank[0]} />
+      <Link to="/dayrating/일요일"><div className="rank"/></Link>
+      </div>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>월</h3></div>
-      <Rating />
-      <Link to="/dayrating/월요일"><Button /></Link>
+      <Rating rank={rank[1]}/>
+      <Link to="/dayrating/월요일"><div className="rank"/></Link>
     </div>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>화</h3></div>
-      <Rating />
-      <Link to="/dayrating/화요일"><Button /></Link>
+      <Rating rank={rank[2]}/>
+      <Link to="/dayrating/화요일"><div className="rank"/></Link>
     </div>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>수</h3></div>
-      <Rating />
-      <Link to="/dayrating/수요일"><Button /></Link>
+      <Rating rank={rank[3]}/>
+      <Link to="/dayrating/수요일"><div className="rank"/></Link>
     </div>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>목</h3></div>
-      <Rating />
-      <Link to="/dayrating/목요일"><Button /></Link>
+      <Rating rank={rank[4]}/>
+      <Link to="/dayrating/목요일"><div className="rank"/></Link>
     </div>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>금</h3></div>
-      <Rating />
-      <Link to="/dayrating/금요일"><Button /></Link>
+      <Rating rank={rank[5]}/>
+      <Link to="/dayrating/금요일"><div className="rank"/></Link>
     </div>
     <div className='wrap'>
       <div><h3 style={{marginRight:"20px", marginLeft:"20px"}}>토</h3></div>
-      <Rating />
-      <Link to="/dayrating/토요일"><Button /></Link>
+      <Rating rank={rank[6]}/>
+      <Link to="/dayrating/토요일"><div className="rank"/></Link>
     </div>
     <div>평균 평점</div>
-    <div><Average /></div>
-    <div><button onClick={reset} className='mybtn'>Reset</button></div>
+    <div>{ave}</div>
+    <div><button onClick={() =>{reset()}} className='mybtn'>Reset</button></div>
     </div>
+
+    
     </>
+    
   )
+  
 }
 
 export default Home;
--- src/Rating.js
+++ src/Rating.js
@@ -1,36 +1,27 @@
-import React, {useState} from "react";
+import React from "react";
 import './style.css'
-import Average from "./Average";
-import Home from "./Home";
-
-function Rating(){
-  
-  const getRandom = Math.floor(Math.random() * 6);
-  const [rank, setRank] = useState(getRandom);
-  //console.log(getRandom(0, 5))
-  const star_count = Array.from({length: 5-rank}, (i)=> i);
- console.log(rank)
-  const star_count2 = Array.from({length: rank}, (j)=> j);
-  //console.log(star_count2)
-
-  // const average = (rank)=>{
-  //   const randomNum = [0,0,0]
-  //   const randomArr = randomNum.map((a)=>(a = this.rank))
-  //   console.log(this.rank)}
-
-  return(
-      <div>
-          {star_count2.map((n,j) =>{
-            return <div key={j} id={j} className="circle2"/>
-    })}
-           {star_count.map((n,i) =>{
-            return  <div key={i} id={i} className="circle"/>
-           
-    })} 
-   
-    
-     
-      </div>
+//  const Yoil = () => {
+//             const dayList = ['일','월','화','수','목','금','토']
+//             let day = new Date().getDay()
+//             const yoil = dayList[day+1]
+//            console.log(yoil)}  
+function Rating(props) {
+           console.log(props.rank)
+  return (
+    <div>
+        <div className="wrap">
+         {Array.from({length :(props.rank)}).map((n,i) =>{
+                     return <div key={i}  className="circle2"/>})}
+         {Array.from({length :(5-props.rank)}).map((n,i) =>{
+                     return <div key={i}  className="circle"/>})}
+        </div>
+           {/* <div className="circle2"/>
+            {circle_count2.map((n,i) =>{
+              return <div key={i}  className="circle2"/>})}
+            {circle_count.map((b,i) =>{
+              return  <div key={i}  className="circle"/>})} */}
+         
+    </div>
   )
 
 }
